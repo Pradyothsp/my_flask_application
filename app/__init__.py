ba9dc33e7768b8a2bd05020601362c6599baf524
Pradyothsp <sppradyoth@outlook.com>
2024-09-11 17:52:56 -0400

    Refactor app initialization and API blueprint registration, and add routes blueprint

--- app/__init__.py
+++ app/__init__.py
@@ -5,6 +5,8 @@ from .models import db
 from .api.api_v1 import api_v1
 from .config import Config
 
+from . import routes
+
 migrate = Migrate()
 
 
@@ -18,5 +20,8 @@ def create_app(config_class=Config):
 
     # Register Blueprints
     app.register_blueprint(api_v1)
+
+    # routes
+    app.register_blueprint(routes.bp)
 
     return app
